d88fee5263a42baba3a195787e1f78d1ef5f4ddf
Grzegorz Kulesza <grzegorz.kulesza@zonifero.com>
2024-05-13 18:50:21 +0200

    first commit

---
+++ Keylapp/Keylapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Keylapp
+//
+//  Created by Grzegorz Kulesza on 13/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Keylap")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Keylapp/Keylapp/KeylappApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeylappApp.swift
+//  Keylapp
+//
+//  Created by Grzegorz Kulesza on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct KeylappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
